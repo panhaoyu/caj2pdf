a7758cc44c0270981c7842b3167795e25ca85488
panhaoyu <panhaoyu.china@outlook.com>
2022-08-26 18:38:25 +0800

    调整主脚本

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ for f in base_dir.glob('*.caj'):
     dst = f.parent / f'{f.stem}.pdf'
     if dst.exists():
         continue
+    print(f'Processing: {f.stem}')
     try:
         CAJParser(str(f)).convert(str(dst))
+        print(f'Processed: {f.stem}')
     except:
         print(f'Failed: {f.stem}')
         # raise
